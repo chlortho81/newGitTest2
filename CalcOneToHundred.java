77b97d4503fd66d6ddc8146834e9cee3bd718a71
Aaron Whittle <whittle.a@husky.neu.edu>
2015-05-19 10:17:00 -0400

    Program to sum all the numbers from 1 to 100

---
+++ CalcOneToHundred.java
@@ -0,0 +1,13 @@
+class OneToHundred {
+
+	public void static main(Strings [] args) {
+
+		int start = 1;
+		int sum = 0;
+		while (start < 101) {
+			sum += start;
+			start++;
+		}
+		System.out.println(sum);
+	}
+}
